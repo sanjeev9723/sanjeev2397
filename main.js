e34eae7d8407b9d64aaddba21a6401eb73996291
sanjeev2397 <sanjeev9723@gmail.com>
2022-09-18 23:49:37 +0530

    verify

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
-// set inital value to zero
 let count = 0;
-// select value and buttons
 const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn");
-
 btns.forEach(function (btn) {
   btn.addEventListener("click", function (e) {
     const styles = e.currentTarget.classList;
